22c38d5fe64baaed0eb052d6e1af5e67ab04e179
David Ghedini <david@davidghedini.com>
2023-04-02 18:10:48 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@ from sphinx.locale import _
 from sphinx_rtd_theme import __version__
 
 
-project = u'AcuGIS PostGIS Builder'
-copyright = u'2020, Cited, Inc.'
+project = u'PostGIS Choropleth Maps'
+copyright = u'2023, Cited, Inc.'
 slug = re.sub(r'\W+', '-', project.lower())
 version = __version__
 release = __version__
